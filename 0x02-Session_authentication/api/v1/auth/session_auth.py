a6901123c37c4460098ffbd681f06de98d92a67e
James Kariuki <karisjaymo99@gmail.com>
2024-06-05 17:42:34 +0000

    Task 1, Empty SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+auth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session Auth API Authentication class."""
+    pass
