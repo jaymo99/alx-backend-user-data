c7b5d1bb7030d50c93b33f17c5091aedaa579326
James Kariuki <karisjaymo99@gmail.com>
2024-06-06 02:26:37 +0000

    Task 3, Get  for

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,9 +13,18 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None) -> str:
         """
-        Creates a session ID for a `user_id`"""
+        Creates a session ID for a `user_id`
+        """
         if not user_id or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a `User ID` based on a `Session ID`.
+        """
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)
