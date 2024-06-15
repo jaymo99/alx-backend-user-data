161bd001d5123a09d1366dd561ff1ab383fa8e0b
James Kariuki <karisjaymo99@gmail.com>
2024-06-15 20:46:43 +0000

    Task 18, Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,3 +88,16 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates a user's password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None
+                                 )
+        except NoResultFound:
+            raise ValueError
+        return None
