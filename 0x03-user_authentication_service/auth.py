3621b08a3a65b781a5cd148996f15a4540bcfd62
James Kariuki <karisjaymo99@gmail.com>
2024-06-15 18:58:19 +0000

    Task 8, Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Auth module
 """
-from bcrypt import gensalt, hashpw
+from bcrypt import checkpw, gensalt, hashpw
 from sqlalchemy.orm.exc import NoResultFound
 
 from db import DB
@@ -30,3 +30,13 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Checks if a user exists and password matches
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return checkpw(password.encode('utf-8'), user.hashed_password)
